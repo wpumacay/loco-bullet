3325dbda41824888432c581f2cd1bb9205e09a41
wpumacay <wpumacay@gmail.com>
2019-08-14 22:40:51 -0500

    [Maintenance]: replaced submodule dependencies
    Changes:
    
    * Replaced all git-submodules used as dependencies
      with plain repo clones using helper scripts.

---
+++ scripts/status_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# --------------------------------------------------------------------- #
+# script/setup: Sets up this repo by cloning all required dependencies  #
+# --------------------------------------------------------------------- #
+
+for repo in ext/bullet3 ext/cat1 ext/imgui ext/pybind11 core
+do
+    echo "Checking status: ${repo}"
+    cd "${repo}" 
+    git status 
+    cd "../.."
+    echo "----------------------------------------------------"
+done
