904e7b0f9de865dd6f4e3d8884e625995d95ad8b
wpumacay <wpumacay@gmail.com>
2019-08-15 00:54:37 -0500

    [Maintenance]: cleaning cached referenced submods
    Changes:
    
    * Removed cached submodules from git to fix issues
      with --recursive when using this repo as dep.

--- scripts/setup_dependencies.sh
+++ scripts/setup_dependencies.sh
@@ -4,6 +4,14 @@
 # script/setup: Sets up this repo by cloning all required dependencies  #
 # --------------------------------------------------------------------- #
 
+echo "======== Installing apt-dependencies ============"
+
+echo "==> Installing build tools"
+sudo apt-get install make cmake pkg-config
+
+echo "==> Installing dependencies for renderer"
+sudo apt-get install libassimp-dev libglfw3-dev libglew-dev
+
 echo "======== Cloning dependencies -> ext/ ==========="
 
 # rendering engine for visualizer
